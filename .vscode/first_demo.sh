66359d93d8c452cbd2e9ed9d66ace8b5584b1d5c
mytool2002 <mytool2002@gmail.com>
2022-07-19 15:53:21 +0800

    我编写第一个shell脚本

---
+++ .vscode/first_demo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+varA=1
+varB=2
+echo $varA
+echo $varB
--- getopt_demo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-parameters=`getopt -o ab:c:: --long add:,remove::,show -n "$0" -- "$@"`
-echo "$parameters"
-
